f8ab7793700b147cf3b133e50e81c9b0b8ff19a7
Malik Browne <browne0@purdue.edu>
2016-09-15 00:13:21 -0400

    Okay im a perfectionist and now first page done
    i try too hard

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-
-typer('.typertext', 55)
+typer('.typertext', 45)
 	.cursor({blink: 'soft'})
 	.line("I'd like to say that I'm a pretty skilled Web Developer.")
 	.pause(2000)
@@ -12,9 +11,23 @@ typer('.typertext', 55)
 	.back(13)
 	.continue("Designer.")
 	.pause(2000)
+	.back(9)
+	.continue("Ukulele player.")
+	.pause(2000)
 	.back('all')
-	.continue("I'll do whatever you need, really. Can't help it, I'm an overachiever.")
+	.continue("I can do whatever you need, really. Can't help it, I'm an overachiever.")
 	.pause(5000)
 	.empty()
 	.pause(300)
 	.continue("Web Developer, iOS Developer, Designer, Photographer. The story continues!");
+
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+    	forceHeight: false,
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            // console.log(data.curTop);
+        }
+    });
+} )( jQuery );
